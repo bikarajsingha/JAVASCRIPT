67553d24c1382e1a0867e35d37e9b46e52fcd71b
bikarajsingha <bikarajsingha@gmail.com>
2021-11-29 20:07:57 +0530

    for

--- Problems/dom.js
+++ Problems/dom.js
@@ -1,29 +1,7 @@
-//2
-// Examine the document object //
-// console.dir(document)
+var items = document.getElementsByClassName('list-group-item')
 
-console.log(document.domain)
-console.log(document.URL)
-console.log(document.title)
-document.title = '12345'
-console.log(document.doctype)
-console.log(document.all)
-console.log(document.all[10])
-//document.all[10].textContent ='hello'
-console.log(document.forms)
-console.log(document.images)
-console.log(document.links)
+items[2].style.backgroundColor = 'green'
 
-//GetElement
-let headerTitle = document.getElementById('header-title')
-let header = document.getElementById('main-header')
-//console.log(headerTitle)
-//headerTitle.textContent = 'Hello'
-//headerTitle.style.borderBottom = 'solid 3px #000'
-
-header.style.borderBottom = 'solid 3px #000'
-
-//3
-let listHeader = document.getElementsByClassName('title')
-listHeader[0].style.color = 'green'
-listHeader[0].innerText = 'ADD ITEM'
+for(let i of items){
+    i.style.fontWeight = 'bold'
+}
