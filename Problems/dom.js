020664b59609a07c8b52ea7f2c059f7f3c18a89a
bikarajsingha <bikarajsingha@gmail.com>
2021-11-30 02:14:43 +0530

    dom manupilation

--- Problems/dom.js
+++ Problems/dom.js
@@ -21,10 +21,55 @@ var titles = document.querySelectorAll('.title')
 console.log(titles)
 titles[0].textContent = 'hello'*/
 
-let item = document.querySelectorAll('.list-group-item')
+/*let item = document.querySelectorAll('.list-group-item')
 item[1].style.color = 'green'
 
 let oddItems = document.querySelectorAll('.list-group-item:nth-child(odd)')
 
 for(let i of oddItems)
-    i.style.backgroundColor = 'green'
+    i.style.backgroundColor = 'green'*/
+
+var itemList = document.querySelector('#items')
+itemList.parentElement.style.backgroundColor = '#f4f4f4'
+
+itemList.children[1].style.backgroundColor = 'yellow'
+
+console.log(itemList.firstChild)
+
+console.log(itemList.firstElementChild)
+itemList.firstElementChild.textContent = 'Hello 1'
+
+console.log(itemList.lastChild)
+
+console.log(itemList.lastElementChild)
+itemList.lastElementChild.textContent = 'Hello 2'
+
+console.log(itemList.nextSibling)
+console.log(itemList.nextElementSibling)
+console.log(itemList.previousSibling)
+itemList.previousElementSibling.style.color = 'green'
+
+let newDiv = document.createElement('div')
+newDiv.className = 'hello'
+newDiv.id = 'helloId'
+newDiv.setAttribute('title', 'hello Div')
+
+var newDivText = document.createTextNode('HELLo World')
+newDiv.appendChild(newDivText)
+
+var container = document.querySelector('header .container')
+var h1 = document.querySelector('header h1')
+
+container.insertBefore(newDiv, h1)
+
+let newp = document.createElement('p')
+let newPText = document.createTextNode('HELLo World')
+newp.appendChild(newPText)
+
+var ul = document.querySelector('ul')
+var li = document.querySelector('li')
+ul.insertBefore(newp, li)
+
+console.log(newp)
+
+
