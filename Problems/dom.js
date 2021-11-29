4be140bfcbf4fa65101380c4eeb4d0e28ddb54b4
bikarajsingha <bikarajsingha@gmail.com>
2021-11-30 00:03:48 +0530

    querySelectorAll

--- Problems/dom.js
+++ Problems/dom.js
@@ -1,5 +1,30 @@
-let list = document.getElementsByClassName('new-class')
-list[0].style.fontWeight = 'bold'
+/*var header = document.querySelector('#main-header')
+header.style.borderBottom = 'solid 4px #ccc'
 
-let item = document.getElementsByTagName('li')
-item[4].style.backgroundColor = 'pink'
+var input = document.querySelector('input')
+input.value = 'Hello world'
+
+var submit = document.querySelector("input[type='submit']")
+submit.value = 'SEND'
+
+var item = document.querySelector('.list-group-item')
+item.style.color = 'red'
+
+var lastItem = document.querySelector('.list-group-item:last-child')
+lastItem.style.color = 'blue'
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)')
+secondItem.style.color = 'coral'
+
+// querySelector //
+var titles = document.querySelectorAll('.title')
+console.log(titles)
+titles[0].textContent = 'hello'*/
+
+let item = document.querySelectorAll('.list-group-item')
+item[1].style.color = 'green'
+
+let oddItems = document.querySelectorAll('.list-group-item:nth-child(odd)')
+
+for(let i of oddItems)
+    i.style.backgroundColor = 'green'
