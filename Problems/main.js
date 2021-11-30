ada0dcaa794fdb6350e9efd9f99341aa1b55950b
bikarajsingha <bikarajsingha@gmail.com>
2021-11-30 17:12:55 +0530

    store in right format

--- Problems/main.js
+++ Problems/main.js
@@ -8,6 +8,21 @@ function storeLocal(e){
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
     
-    localStorage.setItem('name', name)
-    localStorage.setItem('email', email)
+    let obj = {
+        'name': name,
+        'email': email
+    }
+    objSerialize = JSON.stringify(obj)
+    localStorage.setItem('objItem', objSerialize)
+}
+
+function viewStorage(){
+    let obj = JSON.parse(localStorage.getItem('objItem'))
+
+    let p = document.createElement('p')
+    p.appendChild(document.createTextNode(obj.name+' '+obj.email))
+    p.className = 'btn'
+
+    let body = document.querySelector('body')
+    body.appendChild(p)
 }
