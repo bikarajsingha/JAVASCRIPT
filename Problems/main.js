e9224fff55315d861f2779eab3906faa18e3a2b3
bikarajsingha <bikarajsingha@gmail.com>
2021-11-30 23:53:53 +0530

    Delete edit from local storage

--- Problems/main.js
+++ Problems/main.js
@@ -1,13 +1,22 @@
 let submit = document.querySelector('input[type="submit"]')
 
+let editBtn = document.createElement('button')
+editBtn.appendChild(document.createTextNode('E'))
+
+let delBtn = document.createElement('button')
+delBtn.appendChild(document.createTextNode('X'))
+
 submit.addEventListener('click', storeLocal)
 
 function storeLocal(e){
     e.preventDefault();
-
+    
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
     
+    if(localStorage.getItem('user '+name)){
+        localStorage.removeItem('user '+name)
+    }
     let obj = {
         'name': name,
         'email': email
@@ -15,11 +24,34 @@ function storeLocal(e){
     let objSerialize = JSON.stringify(obj)
     localStorage.setItem('user '+name, objSerialize)
 
-    let objDeserialize = JSON.parse(localStorage.getItem('user '+name))
-    let li = document.createElement('li')
     
+    let objDeserialize = JSON.parse(localStorage.getItem('user '+name))
+
+    let li = document.createElement('li')
     li.appendChild(document.createTextNode(objDeserialize.name+' '+objDeserialize.email))
     
+    delBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        localStorage.removeItem('user '+name)
+
+        let form = document.querySelector('form')
+        let item = e.target.parentElement
+
+        form.removeChild(item)
+    })
+
+    editBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        let form = document.querySelector('form')
+        let item = e.target.parentElement
+
+        form.removeChild(item)
+    })
+
+    li.appendChild(editBtn)
+    li.appendChild(delBtn)
+
     let form = document.querySelector('form')
     form.appendChild(li)
 
@@ -35,7 +67,34 @@ window.addEventListener('DOMContentLoaded', (e) => {
             break
     
         li.appendChild(document.createTextNode(objDeserialize.name+' '+objDeserialize.email))
+
+        delBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            localStorage.removeItem(key)
     
+            let form = document.querySelector('form')
+            let item = e.target.parentElement
+    
+            form.removeChild(item)
+        })
+        
+        editBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            
+            let inputName = document.getElementById('name')
+            let inputEmail  = document.getElementById('email')
+
+            
+
+            let form = document.querySelector('form')
+            let item = e.target.parentElement
+    
+            form.removeChild(item)
+        })
+
+        li.appendChild(editBtn)
+        li.appendChild(delBtn)
+
         let form = document.querySelector('form')
         form.appendChild(li)
     }
