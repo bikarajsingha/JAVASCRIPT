842b33d472305294bcf65c83261daa792fe40064
bikarajsingha <bikarajsingha@gmail.com>
2021-12-20 18:46:57 +0530

    callApplyBind

--- Problems/callApplybind.js
+++ Problems/callApplybind.js
@@ -1,1 +1,34 @@
-A
+//Call
+
+var obj = {
+    num: 2
+}
+
+var addToThis = function(a, b, c){
+    return this.num + a + b + c
+}
+
+console.log(addToThis.call(obj, 1, 2, 3))
+
+//Apply
+
+var arr = [4, 5, 6]
+console.log(addToThis.apply(obj, arr))
+
+//Bind
+
+var bound = addToThis.bind(obj)
+console.log(bound(1, 3, 5))
+
+//4
+
+var student = {
+    age: 20
+}
+
+var print = function(){
+    console.log(this.age)
+}
+
+var bound2 = print.bind(student)
+bound2()
