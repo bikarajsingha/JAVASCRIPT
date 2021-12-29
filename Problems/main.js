e4bd5752a14e3f661925c3d79c37b4c2b593132a
bikarajsingha <bikarajsingha@gmail.com>
2021-12-29 12:58:45 +0530

    AXIOS & CRUD (POST)

--- Problems/main.js
+++ Problems/main.js
@@ -14,89 +14,13 @@ function storeLocal(e){
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
     
-    if(localStorage.getItem('user '+name)){
-        localStorage.removeItem('user '+name)
-    }
     let obj = {
         'name': name,
         'email': email
     }
-    let objSerialize = JSON.stringify(obj)
-    localStorage.setItem('user '+name, objSerialize)
-
     
-    let objDeserialize = JSON.parse(localStorage.getItem('user '+name))
-
-    let li = document.createElement('li')
-    li.appendChild(document.createTextNode(objDeserialize.name+' '+objDeserialize.email))
-    
-    delBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        localStorage.removeItem('user '+name)
-
-        let form = document.querySelector('form')
-        let item = e.target.parentElement
-
-        form.removeChild(item)
-    })
-
-    editBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-
-        let form = document.querySelector('form')
-        let item = e.target.parentElement
-
-        form.removeChild(item)
-    })
-
-    li.appendChild(editBtn)
-    li.appendChild(delBtn)
-
-    let form = document.querySelector('form')
-    form.appendChild(li)
-
+    axios.post('https://crudcrud.com/api/57b4ab41bac14b47963a25d21bdf0dad/appointmentData', obj)
+     .then(response => console.log(response))
+     .catch(err => console.log(err))
 }
 
-
-window.addEventListener('DOMContentLoaded', (e) => {
-    for(let key in localStorage){
-        let li = document.createElement('li')
-        let objDeserialize = JSON.parse(localStorage[key])
-    
-        if(objDeserialize.name == undefined)
-            break
-    
-        li.appendChild(document.createTextNode(objDeserialize.name+' '+objDeserialize.email))
-
-        delBtn.addEventListener('click', (e) => {
-            e.preventDefault()
-            localStorage.removeItem(key)
-    
-            let form = document.querySelector('form')
-            let item = e.target.parentElement
-    
-            form.removeChild(item)
-        })
-        
-        editBtn.addEventListener('click', (e) => {
-            e.preventDefault()
-            
-            let inputName = document.getElementById('name')
-            let inputEmail  = document.getElementById('email')
-
-            inputName.value = objDeserialize.name
-            inputEmail.value = objDeserialize.email
-
-            let form = document.querySelector('form')
-            let item = e.target.parentElement
-    
-            form.removeChild(item)
-        })
-
-        li.appendChild(editBtn)
-        li.appendChild(delBtn)
-
-        let form = document.querySelector('form')
-        form.appendChild(li)
-    }
-})
