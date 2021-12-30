7afcf4ffbfa794945c7479ab54b65729c9884966
bikarajsingha <bikarajsingha@gmail.com>
2021-12-30 23:18:58 +0530

    preOrderTraverse

--- Problems/binarySearchTree.js
+++ Problems/binarySearchTree.js
@@ -1,5 +1,5 @@
 class Node{
-    constructor(data, left=null, right=null){
+    constructor(data, left = null, right = null){
         this.data = data
         this.left = left
         this.right = right
@@ -10,52 +10,79 @@ class BST{
     constructor(){
         this.root = null
     }
-    insert(item){
+    insert(data){
         const node = this.root
         if(node === null){
-            this.root = new Node(item)
-            return 
-        }else {
-            const searchTree = function(item){
-                if(item < node.data){
-                    if(node.left === null){
-                        node.left = new Node(item)
+            this.root = new Node(data)
+            return
+        }else{
+            function searchTree(n){
+                if(data > n.data){
+                    if(n.right === null){
+                        n.right = new Node(data)
                         return
                     }else {
-                        return searchTree(item.left)
+                        return searchTree(n.right)
                     }
-                }else if(item > node.data){
-                    if(node.right === null){
-                        node.right = new Node(item)
+                }else if(data < n.data){
+                    if(n.left === null){
+                        n.left = new Node(data)
                         return
                     }else {
-                        return searchTree(item.right)
+                        return searchTree(n.left)
                     }
                 }else {
                     return null
                 }
             }
-            return searchTree(item)
+            return searchTree(node)
         }
     }
-    search(item){
+    search(data){
         let node = this.root
 
-        while(node.data !== item){
-            if(node.data > item){
-                node = node.left
-            }else {
+        while(node.data != data){
+            if(data > node.data){
                 node = node.right
+            }else {
+                node = node.left
             }
-            if(node === null) return null
+            if(node == null) return node
         }
         return node
+    }
+    preOrderRecursive(){
+        const node = this.root
+        const res = []
+
+        function recursion(n){
+            if(n == null) return 
+
+            res.push(n.data)
+            recursion(n.left)
+            recursion(n.right)
+        }
+
+        recursion(node)
+        return res
+    }
+    preOrderIterative(){
+        const stack = []
+        const res = []
+
+        if(this.root !== null) stack.push(this.root)
+
+        while(stack.length > 0){
+            let node = stack.pop()
+            res.push(node.data)
+
+            if(node.right) stack.push(node.right)
+            if(node.left) stack.push(node.left)
+        }
+        return res
     }
 }
 
 const bTree = new BST()
-bTree.insert(50)
-bTree.insert(75)
-bTree.insert(25)
 
-console.log(bTree.search(7))
+console.log(bTree.preOrderIterative())
