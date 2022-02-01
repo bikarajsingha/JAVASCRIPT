5989a7da770fc904b6055ed76ea95860b12867e6
bikarajsingha <bikarajsingha@gmail.com>
2022-02-02 00:09:52 +0530

    javascript animations

---
+++ toast-notification/script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('btn')
+const container = document.getElementById('container')
+
+console.log(btn, container)
+btn.addEventListener('click', () => {
+    createNotification()
+})
+
+function createNotification() {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText = 'This challenge is completed'
+    container.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 2000)
+}
